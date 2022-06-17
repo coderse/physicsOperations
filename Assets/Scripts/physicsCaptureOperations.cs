2161d4ad988f037b46f40511e1767a8824e5ac50
Ahmet Keskin <ahmetkeskin.se@gmail.com>
2022-06-17 17:50:00 +0300

    Added rigidbody and collider interactions

--- Assets/Scripts/physicsCaptureOperations.cs
+++ Assets/Scripts/physicsCaptureOperations.cs
@@ -4,15 +4,91 @@ using UnityEngine;
 
 public class physicsCaptureOperations : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+    //rigitbody yardýmýyla etkileþimi yakalamak
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log("Etkileþim oldu"); //herhangi bir colliderla herhangi bir objeyle etkileþim olduðunda haber verir.
+
+    }*/
+    
+    //spesifik olarak bir objeyle olan etkileþimi yakalamak için
+    private void OnCollisionEnter(Collision collision)
+    {
+
+       
+
+        if(collision.gameObject.name == "2")
+        {
+            /*Destroy(gameObject); // mesela etkileþim olduðunda scritin olduðu obje yokedilebilir..
+                                 // örnek;yapýlabilecekler..*/
+            Destroy(collision.gameObject); // etkileþim yapýldýðý objeyi yok eder
+
+            Debug.Log("Etkileþim oldu"); //bu þekilde ismi 2 olan objeye dokunduðunda yakalamýþ oluruz
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay(Collision collision)
     {
-        
+        if (collision.gameObject.name == "2")
+        {
+            Debug.Log("Etkileþim sürüyor"); //etkileþim devam ediyorsa
+        }
+
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+            if (collision.gameObject.name == "2")
+            {
+                Debug.Log("Etkileþim bitti"); //etkileþim bittiyse
+            }
+          
+    }
+
+    /*
+   
+    //collider yardýmýyla rigitbodysiz ve fizik kurallarý olmadan etkileþim olduðunu kod tarafýna iletme
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "2")
+        {
+            Debug.Log("Etkileþim oldu"); //bu þekilde ismi 2 olan objeye dokunduðunda yakalamýþ oluruz
+        }
+
+        //tag ile yakalama
+        if (other.CompareTag("FizikYakalamaTag"))
+        {
+            Debug.Log("Etkileþim oldu"); //bu þekilde ismi 2 olan objeye dokunduðunda yakalamýþ oluruz
+        }
+
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "2")
+        {
+            Debug.Log("Etkileþim sürüyor"); //etkileþim devam ediyorsa
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "2")
+        {
+            Debug.Log("Etkileþim bitti"); //etkileþim bittiyse
+        }
+    }
+    */
+  
+
+
+
+
+
+
+
 }
